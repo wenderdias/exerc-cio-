9ac169df8f022934e630879356cf01146d42cb59
wenderdias <146007476+wenderdias@users.noreply.github.com>
2023-11-27 14:58:33 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("olá")
+
+let meuNome = "Weevil";
+let minhaIdade = 38;
+
+
+console.log(meuNome);
+console.log(minhaIdade);
+
+let idadeVezesSete = minhaIdade * 7;
+
+let minhasCaracteristicas = {
+  nome: meuNome,
+  idade: minhaIdade,
+  estudante: true
+};
+
+let amigos = ["João", "Manuel", "Pedro"]
+
+let amigoscaracterísticas = [
+  { nome: "João", características: "calmo, gordo, alto" },
+  { nome: "Manuel", características: "magro, baixo, cabelo grande" },
+  { nome: "Pedro", características: "não tem um braço, nem uma perna,boca grande" }
+];
+
+
+let objetoJoao = { objeto: "bola de futebol" };
+let objetoManuel = { objeto: "🔪" };
+let objetoPedro = { objeto: "um rato" };
+
+
+console.log(idadeVezesSete);
+console.log(minhasCaracteristicas);
+console.log(amigos[0]);
+console.log(amigos[1]);
+console.log(amigos[2]);
+console.log(amigoscaracterísticas[0]);
+console.log(amigoscaracterísticas[1]);
+console.log(amigoscaracterísticas[2]);
+console.log(objetoJoao);
+console.log(objetoManuel);
+console.log(objetoPedro);
